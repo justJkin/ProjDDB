3627b572f52e15208e692dea937fd2739b1c4f4a
Domin <s.domin82@gmail.com>
2024-06-11 03:39:08 +0200

    ##

--- financialApp/Interfaces/IUserService.cs
+++ financialApp/Interfaces/IUserService.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 public interface IUserService
-{
+{ 
+    //nig
     IEnumerable<User> GetAllUsers();
     User GetUserById(int id);
     void CreateUser(User user);
